13d2aa7d57af54a0455671052822f7ee83de23dc
Meena1903 <meenabj03@gmail.com>
2024-05-13 15:14:55 +0530

    example for handling data from diffrent datasets

--- quiz/src/main/java/com/example/quiz/controller/QuestionController.java
+++ quiz/src/main/java/com/example/quiz/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.example.quiz.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,12 +22,12 @@ public class QuestionController {
     QuestionService quizService;
 
     @GetMapping("/allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return quizService.getAllQuestions();
     }
 
     @GetMapping("/category/{category}")
-    public List<Question> getQuestionByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable String category){
         return quizService.getQuestionByCategory(category);
     }
 
--- quiz/src/main/java/com/example/quiz/service/QuestionService.java
+++ quiz/src/main/java/com/example/quiz/service/QuestionService.java
@@ -1,8 +1,11 @@
 package com.example.quiz.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.quiz.dao.QuestionDao;
@@ -14,12 +17,21 @@ public class QuestionService {
     @Autowired
     QuestionDao questionDao;
 
-    public List<Question> getAllQuestions(){
-        return questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        try{
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);   
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> getQuestionByCategory(String category) {
-        return questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category),HttpStatus.OK);
+            
+        } catch (Exception e) {
+        }
     }
 
     public Question addQuestion(Question question) {
