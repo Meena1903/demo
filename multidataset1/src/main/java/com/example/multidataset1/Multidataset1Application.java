982ef5f2fb2ccb3a941de2ad1d57b055b0916c2d
Meena1903 <meenabj03@gmail.com>
2024-05-14 07:45:39 +0530

    trying the same but different idea - multi dataset access from the same databse but different schema

---
+++ multidataset1/src/main/java/com/example/multidataset1/Multidataset1Application.java
@@ -0,0 +1,13 @@
+package com.example.multidataset1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Multidataset1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Multidataset1Application.class, args);
+	}
+
+}
---
+++ multidataset1/src/test/java/com/example/multidataset1/Multidataset1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multidataset1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Multidataset1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
