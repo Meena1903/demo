0a0a028cee024df49eb4e8ba2b3413bde6051eca
Meena1903 <meenabj03@gmail.com>
2024-05-15 13:20:45 +0530

    This is the program for accessing data from different schema in the same database

--- multidatasource/src/main/java/com/easy2excel/springbootwithmultipledatasource/multidatasourceApplication.java
+++ multidatasource/src/main/java/com/easy2excel/springbootwithmultipledatasource/multidatasourceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootWithMultipleDatasourceApplication {
+public class multidatasourceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootWithMultipleDatasourceApplication.class, args);
+		SpringApplication.run(multidatasourceApplication.class, args);
 	}
 
 }
