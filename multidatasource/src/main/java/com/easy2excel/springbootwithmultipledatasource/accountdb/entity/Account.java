222a18ab931ad85aa5bd4129085f356d91bbab38
Meena1903 <meenabj03@gmail.com>
2024-05-15 12:53:56 +0530

    git it after a few tries, worth it

---
+++ multidatasource/src/main/java/com/easy2excel/springbootwithmultipledatasource/accountdb/entity/Account.java
@@ -0,0 +1,31 @@
+package com.easy2excel.springbootwithmultipledatasource.accountdb.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "account")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "website")
+	private String website;
+
+	
+}
---
+++ multidatasource/src/main/java/com/easy2excel/springbootwithmultipledatasource/userdb/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.easy2excel.springbootwithmultipledatasource.userdb.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.easy2excel.springbootwithmultipledatasource.userdb.entity.User;
+import com.easy2excel.springbootwithmultipledatasource.userdb.repository.UserRepository;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("api/v2/user")
+    public User saveUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+    @GetMapping("api/v2/users")
+    public List<User> getUserList(){
+        return userRepository.findAll();
+
+    }
+
+
+}
